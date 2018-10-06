f5b5febd2cea6b4174fc84fb9127e84876ab45d8
Ehabmagdyabbas <ihabmajdyabass@hotmail.com>
2018-10-06 10:40:06 +0200

    Add files via upload

---
+++ nof/connect.php
@@ -0,0 +1,5 @@
+<?php 
+//connect.php;
+$connect = mysqli_connect("mysql.hostinger.in", "u856423732_try", "123456789", "u856423732_messa");
+$cn = mysqli_connect("mysql.hostinger.in", "u856423732_egyba", "9874123951753", "u856423732_bank");
+?>
